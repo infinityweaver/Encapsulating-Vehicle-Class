dbb27c4c113234b8d5e6c50729ca2c15b5035fae
luigilota860 <109124936+luigilota860@users.noreply.github.com>
2022-07-13 16:01:31 +0700

    Lota_Lab2

---
+++ version1/Vehicle.java
@@ -0,0 +1,18 @@
+public class Vehicle
+{
+	public double load;
+	public double maxload;
+
+	public Vehicle(max_load);
+	{
+		this.maxload = max_load;
+	}
+	public double getLoad(
+	{
+		return this.load;
+	}
+	public double getMaxLoad()
+	{
+		return this.maxLoad;
+	}	
+}
