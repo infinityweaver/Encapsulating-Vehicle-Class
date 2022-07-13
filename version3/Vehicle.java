c10fdd2b23cf7ccb5763512446196eec2030f15f
Alvin Macion <macion.et.al@gmail.com>
2022-07-13 16:12:38 +0800

    Update Vehicle.java

--- version3/Vehicle.java
+++ version3/Vehicle.java
@@ -5,29 +5,37 @@ public class Vehicle
 
 	public Vehicle(double maxLoad)
 	{
-		this.maxLoad = maxLoad;
+		this.maxLoad = this.kiloToNewts(maxLoad);
 	}
 
 	public double getLoad()
 	{
-		return this.load;
+		return this.newtsToKilo(load);
 	}
 
 	public double getMaxLoad()
 	{
-		return this.maxLoad;
+		return this.newtsToKilo(maxLoad);
 	}
 
 	public boolean addBox(double weight)
 	{
-		if((weight + load) < maxLoad)
+		if((this.kiloToNewts(weight) + load) < maxLoad)
 		{
-			load += weight;
+			load = load + this.kiloToNewts(weight);
 			return true;
 		}
 		else 
 		{
 			return false;
 		}
+	}
+	private double kiloToNewts (double weight) 
+	{
+		return (weight * 9.8);
+	}
+	private double newtsToKilo (double weight) 
+	{
+		return (weight / 9.8);
 	}
 }
