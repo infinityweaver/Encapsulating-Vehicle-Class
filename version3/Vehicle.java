61e5d12736a5d0d97c4b084a2664faaf4a0db035
senpnaiXiao <cabalesmilchzedek@gmail.com>
2022-07-13 15:53:26 +0800

    Codes

---
+++ version3/Vehicle.java
@@ -0,0 +1,44 @@
+public class Vehicle
+{
+		private double load;
+		private double maxLoad;
+
+		public Vehicle(double maxLoad)
+		{
+			this.load = load;
+			this.maxLoad = maxLoad;
+		}
+
+		public double getLoad()
+		{
+			return this.load;
+		}
+
+		public double getMaxLoad()
+		{
+			return this.maxLoad;
+		}
+
+		public boolean addBox(double weight)
+		{
+			if((load + weight) <= this.maxLoad)
+			{
+				this.load += weight;
+				return true;
+			}
+			else return false;
+		}
+
+		private double kiloToNewts (double weight) 
+		{
+
+			return (weight * 9.8);
+
+		}
+		private double newtsToKilo (double weight)
+		{
+
+			return (weight / 9.8);
+
+		}
+}
